3e1e60b2d054e0b565a93db130e2fc1a3a7736b0
Андрей <Andreyadad@gmail.com>
2023-07-16 12:06:29 +0500

    Заново начал решать задачу. Создал массив AuthorPostfix, в переменную RandomPostfix присвоил рандомное значение из восьми возможных с массива выше, создал переменную RandomAvatar и присвоил в нее рандомный путь из восьми возможных и создал объект со свойством avatar и значением из переменной RandomAvatar в map.js

--- js/map.js
+++ js/map.js
@@ -1,5 +1,21 @@
 'use strict';
+/*var option1 = {
+	'author':
+}
 
-var SimilarAdsNearby = [1, 2, 3, 4, 5, 6, 7, 8];
+var SimilarAdsNearby = ['', 2, 3, 4, 5, 6, 7, 8];*/
 
-/*console.log(SimilarAdsNearby[(Math.floor(Math.random() * SimilarAdsNearby.length))]);*/
+
+
+
+var AuthorPostfix = ['01', '02', '03', '04', '05', '06', '07', '08'];
+
+var RandomPostfix = AuthorPostfix[(Math.floor(Math.random() * AuthorPostfix.length))];
+
+var RamdomAvatar = 'img/avatars/user' + RandomPostfix + '.png';
+
+var author = {
+	'avatar': 'RamdomAvatar'
+};
+
+console.log(RamdomAvatar);
